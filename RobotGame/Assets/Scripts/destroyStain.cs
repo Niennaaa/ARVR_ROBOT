67a5ab655abe8a817f5997e74c811d7b96626719
Jehanne <jehanne.sarrazin@yahoo.fr>
2022-12-05 21:11:30 +0100

    Kyle can now wear hats and the broom makes stains disappears

--- RobotGame/Assets/Scripts/destroyStain.cs
+++ RobotGame/Assets/Scripts/destroyStain.cs
@@ -2,27 +2,27 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class allBooksOk : MonoBehaviour
+public class destroyStain : MonoBehaviour
 {
-    public int AllBooksAreGood;
-
-
-    public bool levelDone = false;
-
-    
     // Start is called before the first frame update
     void Start()
     {
-        AllBooksAreGood = 0;
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (AllBooksAreGood == 5)
-        {
-            levelDone = true;
-        }
+        
+    }
 
+   
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Stain")
+        {
+            Destroy(other.gameObject) ;
+        }
     }
 }
